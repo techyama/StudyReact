21d89ca3f65a3a01c29148b5f860311dc620a6c2
i-yamamotopro <yamamoto.ikki.tech@gmail.com>
2024-05-17 15:52:27 +0900

    セクション11終了

--- react-guide-material/11_hooks_p1/src/040_useContext_with_state/start/Example.jsx
+++ react-guide-material/11_hooks_p1/src/040_useContext_with_state/start/Example.jsx
@@ -1,14 +1,16 @@
-import { createContext } from "react";
+import { useState, createContext } from "react";
 import Child from "./components/Child";
 import OtherChild from "./components/OtherChild";
-export const MyContext = createContext("hello");
+export const MyContext = createContext();
 
 const Example = () => {
+  const [ state, setState ] = useState(0);
   return (
-    <>
+    // 囲んだコンポーネントでvalueの値を使えるようになる
+    <MyContext.Provider value={[ state, setState ]}>
       <Child />
       <OtherChild />
-    </>
+    </MyContext.Provider>
   );
 };
 
---
+++ react-guide-material/11_hooks_p1/src/070_practice_useContext/start/context/CalcProvider.jsx
@@ -0,0 +1,51 @@
+import { useReducer, createContext, useContext, Children } from "react";
+
+const CalcContext = createContext();
+const CalcDispatchContext = createContext();
+
+const reducer = (state, { type, payload }) => {
+    switch (type) {
+      case "change": {
+        const { name, value } = payload;
+        return { ...state, [name]: value };
+      }
+      case "add": {
+        return { ...state, result: parseInt(state.a) + parseInt(state.b) };
+      }
+      case "minus": {
+        return { ...state, result: state.a - state.b };
+      }
+      case "divide": {
+        return { ...state, result: state.a / state.b };
+      }
+      case "multiply": {
+        return { ...state, result: state.a * state.b };
+      }
+      default:
+        throw new Error("operator is invalid");
+    }
+  };
+
+const CalcProvider = ({ children }) => {
+    const initState = {
+        a: 1,
+        b: 2,
+        result: 3,
+    };
+    
+    const [state, dispatch] = useReducer(reducer, initState);
+
+    return (
+        <CalcContext.Provider value={state}>
+            <CalcDispatchContext.Provider value={dispatch}>
+                {children}
+            </CalcDispatchContext.Provider>
+        </CalcContext.Provider>
+    );
+};
+
+const useCalc = () => useContext(CalcContext); 
+
+const useCalcDispatch = () => useContext(CalcDispatchContext); 
+
+export { CalcProvider, useCalc, useCalcDispatch };
