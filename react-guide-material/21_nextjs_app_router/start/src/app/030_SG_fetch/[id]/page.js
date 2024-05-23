a0bb76c7dd66d781d1e07e7ffa8a6c89aaa08fcc
i-yamamotopro <yamamoto.ikki.tech@gmail.com>
2024-05-23 23:52:32 +0900

    generateMetadetaでmetadataの作成

--- react-guide-material/21_nextjs_app_router/start/src/app/030_SG_fetch/[id]/page.js
+++ react-guide-material/21_nextjs_app_router/start/src/app/030_SG_fetch/[id]/page.js
@@ -1,6 +1,17 @@
 import { ENDPOINT } from "@/constants";
 import Article from "../../../components/article";
 
+// metadataの動的作成
+export async function generateMetadata({ params }) {
+  const article = await fetch(`${ENDPOINT}/${params.id}`).then((res) =>
+    res.json()
+  );
+  return (
+    title: article.title,
+    description: article.text
+  );
+}
+
 // SGに変更
 export async function generateStaticParams() {
   const data = await fetch(ENDPOINT).then(res => res.json());
