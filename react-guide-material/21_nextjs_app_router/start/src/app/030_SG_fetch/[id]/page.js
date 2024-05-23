acabe8adc3340e3b5552fa877dda356553b2e265
i-yamamotopro <yamamoto.ikki.tech@gmail.com>
2024-05-23 23:43:55 +0900

    generateStaticParamsでSGの作成2

--- react-guide-material/21_nextjs_app_router/start/src/app/030_SG_fetch/[id]/page.js
+++ react-guide-material/21_nextjs_app_router/start/src/app/030_SG_fetch/[id]/page.js
@@ -1,6 +1,12 @@
 import { ENDPOINT } from "@/constants";
 import Article from "../../../components/article";
 
+// SGに変更
+export async function generateStaticParams() {
+  const data = await fetch(ENDPOINT).then(res => res.json());
+  return data.map(record => ({ id: record.id }));
+}
+
 export default async function Detail({ params }) {
   const article = await fetch(`${ENDPOINT}/${params.id}`).then((res) =>
     res.json()
